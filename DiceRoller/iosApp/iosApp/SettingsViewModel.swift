b393f6923780127f7b02610ff8ac9eeced8a9d3e
Kevin Galligan <kgalligan@gmail.com>
2023-09-08 10:13:07 -0400

    Change import to Foundation and remove try from loop

--- DiceRoller/iosApp/iosApp/SettingsViewModel.swift
+++ DiceRoller/iosApp/iosApp/SettingsViewModel.swift
@@ -16,7 +16,7 @@
 
 import shared
 import Combine
-import UIKit
+import Foundation
 
 @MainActor
 final class SettingsViewModel: ObservableObject {
@@ -47,7 +47,7 @@ final class SettingsViewModel: ObservableObject {
     private var currentSettings: DiceSettings? = nil
 
     func startObservingSettings() async {
-         for try await settings in repository.settings {
+         for await settings in repository.settings {
              self.diceCount = Int(settings.diceCount)
              self.sideCount = Int(settings.sideCount)
              self.uniqueRollsOnly = settings.uniqueRollsOnly
