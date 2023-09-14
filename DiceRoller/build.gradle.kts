fa498344a9c3edc2f50456a0c509cbae766d40d9
Kevin Galligan <kgalligan@gmail.com>
2023-09-14 13:15:36 -0400

    Using SKIE (#14)
    * Use SKIE
    
    * Android view model changes
    
    * Change import to Foundation and remove try from loop

--- DiceRoller/build.gradle.kts
+++ DiceRoller/build.gradle.kts
@@ -20,7 +20,6 @@ plugins {
     alias(libs.plugins.kotlin.cocoapods) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.nativecoroutines) apply false
 }
 
 tasks.register<Delete>("clean") {
