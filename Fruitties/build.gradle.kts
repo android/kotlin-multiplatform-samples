d0e57e853c53e1e5826a3383e07dfce556c97ccc
cartland <c.cartland@gmail.com>
2024-08-16 08:57:27 -0700

    Update plugin build syntax in project builld.gradle.kts

--- Fruitties/build.gradle.kts
+++ Fruitties/build.gradle.kts
@@ -15,12 +15,12 @@
  */
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    alias(libs.plugins.androidApplication).apply(false)
-    alias(libs.plugins.androidLibrary).apply(false)
-    alias(libs.plugins.kotlinAndroid).apply(false)
-    alias(libs.plugins.kotlinMultiplatform).apply(false)
-    alias(libs.plugins.compose.compiler).apply(false)
-    alias(libs.plugins.spotless).apply(false)
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.spotless) apply false
 }
 
 subprojects {
