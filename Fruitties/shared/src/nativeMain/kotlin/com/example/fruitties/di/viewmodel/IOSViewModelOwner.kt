6f1f0c3a458631eaf16849bf189f96a6b6bbbe47
cartland <c.cartland@gmail.com>
2024-09-18 11:10:15 -0700

    Simplify IOSViewModelOwner

--- Fruitties/shared/src/nativeMain/kotlin/com/example/fruitties/di/viewmodel/IOSViewModelOwner.kt
+++ Fruitties/shared/src/nativeMain/kotlin/com/example/fruitties/di/viewmodel/IOSViewModelOwner.kt
@@ -1,6 +1,5 @@
 package com.example.fruitties.di.viewmodel
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStore
 import androidx.lifecycle.ViewModelStoreOwner
@@ -8,46 +7,22 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.MutableCreationExtras
 import com.example.fruitties.di.AppContainer
 import com.example.fruitties.viewmodel.MainViewModel
-import kotlin.reflect.KClass
 
 class IOSViewModelOwner(private val appContainer: AppContainer) : ViewModelStoreOwner {
-    private var _viewModelStore: ViewModelStore? = null
-    override val viewModelStore: ViewModelStore
-        get() = _viewModelStore ?: ViewModelStore().also {
-            this._viewModelStore = it
-        }
+    override val viewModelStore: ViewModelStore = ViewModelStore()
 
-    val mainViewModel: MainViewModel = viewModel(
-        modelClass = MainViewModel::class,
-        viewModelStoreOwner = this,
-        factory = MainViewModel.Factory,
-        extras = MutableCreationExtras().apply {
-            set(MainViewModel.APP_CONTAINER_KEY, appContainer)
-        } as CreationExtras,
-    )
+    // Prepare the parameters for MainViewModel with CreationExtras
+    private val mainViewModelExtras: CreationExtras = MutableCreationExtras().apply {
+        set(MainViewModel.APP_CONTAINER_KEY, appContainer)
+    }
 
-//    TODO: Clear the ViewModelStore when going out of scope.
-//    fun clearViewModelStore() {
-//        viewModelStore.clear()
-//    }
-}
+    val mainViewModel: MainViewModel = ViewModelProvider.create(
+        this as ViewModelStoreOwner,
+        MainViewModel.Factory,
+        mainViewModelExtras,
+    )[MainViewModel::class]
 
-private fun <VM : ViewModel> viewModel(
-    modelClass: KClass<VM>,
-    viewModelStoreOwner: ViewModelStoreOwner,
-    key: String? = null,
-    factory: ViewModelProvider.Factory? = null,
-    extras: CreationExtras = CreationExtras.Empty
-): VM {
-    val provider =
-        if (factory != null) {
-            ViewModelProvider.create(viewModelStoreOwner.viewModelStore, factory, extras)
-        } else {
-            ViewModelProvider.create(viewModelStoreOwner)
-        }
-    return if (key != null) {
-        provider[key, modelClass]
-    } else {
-        provider[modelClass]
+    fun clear() {
+        viewModelStore.clear()
     }
 }
