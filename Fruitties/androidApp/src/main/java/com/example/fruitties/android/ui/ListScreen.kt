84efc56dee1b926d4c452adffd90a974a1f88dd1
cartland <c.cartland@gmail.com>
2024-09-26 16:35:01 -0700

    Simplify Scaffold edge-to-edge and topBar
    * Edge-to-edge: Use contentWindowInsets to allow drawing to Bottom edge
    * TopBar: Use CenterAlignedTopAppBar to simplify centering

--- Fruitties/androidApp/src/main/java/com/example/fruitties/android/ui/ListScreen.kt
+++ Fruitties/androidApp/src/main/java/com/example/fruitties/android/ui/ListScreen.kt
@@ -25,11 +25,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.calculateEndPadding
-import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.foundation.lazy.LazyColumn
@@ -38,12 +39,12 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -54,15 +55,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.fruitties.android.LocalTopAppBarColors
 import com.example.fruitties.android.R
 import com.example.fruitties.android.di.App
 import com.example.fruitties.model.CartItemDetails
@@ -92,32 +89,30 @@ fun ListScreen() {
 
     Scaffold(
         topBar = {
-            TopAppBar(
-                colors = LocalTopAppBarColors.current ?: TopAppBarDefaults.topAppBarColors(),
+            CenterAlignedTopAppBar(
                 title = {
-                    Text(
-                        text = stringResource(R.string.frutties),
-                        style = MaterialTheme.typography.titleLarge,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.fillMaxWidth(),
-                    )
-                }
+                    Text(text = stringResource(R.string.frutties),)
+                },
+                colors = TopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    scrolledContainerColor = MaterialTheme.colorScheme.primary,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                    actionIconContentColor = MaterialTheme.colorScheme.onPrimary,
+                ),
             )
         },
+        contentWindowInsets = WindowInsets.safeDrawing.only(
+            // Do not include Bottom so scrolled content is drawn below system bars.
+            // Include Horizontal because some devices have camera cutouts on the side.
+            WindowInsetsSides.Top + WindowInsetsSides.Horizontal
+        ),
     ) { paddingValues ->
         Column(
             modifier = Modifier
                 // Support edge-to-edge (required on Android 15)
                 // https://developer.android.com/develop/ui/compose/layouts/insets#inset-size
-                .padding(
-                    top = paddingValues.calculateTopPadding(),
-                    start  = paddingValues.calculateStartPadding(
-                        layoutDirection = LocalLayoutDirection.current,
-                    ),
-                    end = paddingValues.calculateEndPadding(
-                        layoutDirection = LocalLayoutDirection.current,
-                    ),
-                ),
+                .padding(paddingValues),
         ) {
             var expanded by remember { mutableStateOf(false) }
             Row(modifier = Modifier.padding(16.dp)) {
