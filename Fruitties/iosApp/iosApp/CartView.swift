b1e0e100346e5e2f253c35130dc4f44c6420fc9b
Ben Trengrove <bentrengrove@users.noreply.github.com>
2024-10-03 06:23:54 +1000

    Switch to Observing API in swift (#37)

--- Fruitties/iosApp/iosApp/CartView.swift
+++ Fruitties/iosApp/iosApp/CartView.swift
@@ -21,56 +21,46 @@ import shared
 struct CartView : View {
     let mainViewModel: MainViewModel
 
-    // The ViewModel exposes a StateFlow.
-    // We collect() the StateFlow into State, which can be used in SwiftUI.
-    // https://skie.touchlab.co/features/flows-in-swiftui
-    @State
-    var cartUIState: CartUiState = CartUiState(cartDetails: [])
-
     @State
     private var expanded = false
 
     var body: some View {
-        VStack {
-            HStack {
-                let total = cartUIState.cartDetails.reduce(0) { $0 + ($1.count) }
-                Text("Cart has \(total) items").padding()
-                Spacer()
-                Button {
-                    expanded.toggle()
-                } label: {
-                    if (expanded) {
-                        Text("collapse")
-                    } else {
-                        Text("expand")
-                    }
-                }.padding()
-            }
-            if (expanded) {
-                CartDetailsView(mainViewModel: mainViewModel)
+        // https://skie.touchlab.co/features/flows-in-swiftui
+        Observing(self.mainViewModel.cartUiState) { cartUIState in
+            VStack {
+                HStack {
+                    let total = cartUIState.cartDetails.reduce(0) { $0 + $1.count }
+                    Text("Cart has \(total) items").padding()
+                    Spacer()
+                    Button {
+                        expanded.toggle()
+                    } label: {
+                        if (expanded) {
+                            Text("collapse")
+                        } else {
+                            Text("expand")
+                        }
+                    }.padding()
+                }
+                if (expanded) {
+                    CartDetailsView(mainViewModel: mainViewModel)
+                }
             }
         }
-        // https://skie.touchlab.co/features/flows-in-swiftui
-        .collect(flow: self.mainViewModel.cartUiState, into: $cartUIState)
     }
 }
 
 struct CartDetailsView: View {
     let mainViewModel: MainViewModel
 
-    // The ViewModel exposes a StateFlow.
-    // We collect() the StateFlow into State, which can be used in SwiftUI.
-    // https://skie.touchlab.co/features/flows-in-swiftui
-    @State
-    var cartUIState: CartUiState = CartUiState(cartDetails: [])
-
     var body: some View {
-        VStack {
-            ForEach(cartUIState.cartDetails, id: \.fruittie.id) { item in
-                Text("\(item.fruittie.name): \(item.count)")
+        
+        Observing(self.mainViewModel.cartUiState) { cartUIState in
+            VStack {
+                ForEach(cartUIState.cartDetails, id: \.fruittie.id) { item in
+                    Text("\(item.fruittie.name): \(item.count)")
+                }
             }
         }
-        // https://skie.touchlab.co/features/flows-in-swiftui
-        .collect(flow: mainViewModel.cartUiState, into: $cartUIState)
     }
 }
