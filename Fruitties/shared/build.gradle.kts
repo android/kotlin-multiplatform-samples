04fa19f81fb21bc3195aeec9035ce3d83e4ef198
Jaehwa Noh <shwoghk14@gmail.com>
2025-08-24 09:45:21 +0900

    Add koin core and android libraries.

--- Fruitties/shared/build.gradle.kts
+++ Fruitties/shared/build.gradle.kts
@@ -93,6 +93,8 @@ kotlin {
                 api(libs.androidx.datastore.core.okio)
                 implementation(libs.okio)
                 implementation(libs.kermit)
+                implementation(project.dependencies.platform(libs.koin.bom))
+                implementation(libs.koin.core)
             }
         }
 
@@ -109,6 +111,8 @@ kotlin {
                 // dependencies declared in commonMain.
                 implementation(libs.ktor.client.okhttp)
                 implementation(libs.androidx.room.paging)
+                implementation(project.dependencies.platform(libs.koin.bom))
+                implementation(libs.koin.android)
             }
         }
 
