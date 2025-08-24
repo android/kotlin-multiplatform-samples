c309013fff010dc36c6cf776b9f564a9b132e6f4
Jaehwa Noh <shwoghk14@gmail.com>
2025-08-24 15:45:00 +0900

    Inject koin

--- Fruitties/androidApp/src/main/java/com/example/fruitties/android/FruittiesAndroidApp.kt
+++ Fruitties/androidApp/src/main/java/com/example/fruitties/android/FruittiesAndroidApp.kt
@@ -17,24 +17,18 @@
 package com.example.fruitties.android
 
 import android.app.Application
-import androidx.compose.runtime.staticCompositionLocalOf
-import com.example.fruitties.di.AppContainer
-import com.example.fruitties.di.Factory
+import com.example.fruitties.di.appModule
+import com.example.fruitties.di.multiplatformModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext.startKoin
 
 class FruittiesAndroidApp : Application() {
-    /** AppContainer instance used by the rest of classes to obtain dependencies */
-    lateinit var container: AppContainer
-
     override fun onCreate() {
         super.onCreate()
-        container = AppContainer(Factory(this))
+
+        startKoin {
+            androidContext(this@FruittiesAndroidApp)
+            modules(appModule, multiplatformModule)
+        }
     }
 }
-
-/**
- * Allows retrieving the AppContainer, which represents a DI graph everywhere from a composable.
- * Because the [AppContainer] is effectively a singleton, we can use static composition local,
- * because it won't change during the app execution.
- */
-val LocalAppContainer =
-    staticCompositionLocalOf<AppContainer> { error("No AppContainer provided!") }
